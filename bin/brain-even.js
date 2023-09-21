21756b9adbe714f9b23a3fa415063d46dfe1be46
delon <nistated@gmail.com>
2023-09-21 16:26:53 +0300

    add bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-function number(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-let i = 0;
 do {
+        function number(min, max){
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        let i = 0;
         console.log('Question: ${number(1, 100)}');
         let readlineSync = require('readline-sync');
         let answer = readlineSync.question('Your answer: '); 
