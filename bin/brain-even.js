d20870e84af4e9515c58baf2c2b35132d5d01bd8
delon <nistated@gmail.com>
2023-09-25 13:48:18 +0300

    add README.md

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,23 +1,28 @@
+console.log('Welcome to the Brain Games');
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi, ' + userName + '!');
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 function number(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 let i = 0;
 do {
-        console.log('Question:' + number(1, 100));
+        const numbers = number(1, 100);
+        console.log('Question:' + numbers);
         let readlineSync = require('readline-sync');
         let answer = readlineSync.question('Your answer: '); 
-    if (number % 2 === 0 && answer === 'yes'){
+    if (numbers % 2 === 0 && answer === 'yes'){
         i+= 1;    
         console.log('Correct!');
-} else if (number % 2 !== 0 && answer === 'no'){
+} else if (numbers % 2 !== 0 && answer === 'no'){
         i+= 1;
         console.log('Correct!');
 } else {
-        const  trueAnswer = answer === 'no' ? 'Yes' : 'no';
-        return console.log(answer + 'is wrong answer ;(. Correct answer was' + trueAnswer +'. Lets try again,' + userName + '!');
+        const  trueAnswer = answer === 'no' ? 'yes' : 'no';
+        return console.log(answer + ' is wrong answer ;(. Correct answer was ' + trueAnswer +'. Lets try again, ' + userName + '!');
 }
 } while (i < 3);
 if (i = 3){
-        console.log ('Congratulations,' + userName);
-};
+        console.log ('Congratulations, ' + userName + '!');
+}
