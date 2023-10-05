1ff1981af5c6237a43482ea6670fbf165360d994
delon <nistated@gmail.com>
2023-10-05 16:20:23 +0300

    add bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 console.log('Welcome to the Brain Games');
-var readlineSync = require('readline-sync');
-var userName = readlineSync.question('May I have your name? ');
+const readlineSync = require('readline-sync');
+const userName = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + userName + '!');
 console.log('What is the result of the expression?');
 function number(min, max){
@@ -17,30 +17,28 @@ do {
     const numbers1 = number(1, 100);
     const numbers2 = number(1, 100);
     const symbol = randomSymbols(symbols);
-    let answer = console.log('numbers1 + symbol + numbers2');
-    var readlineSync = require('readline-sync');
-    var userAnswer = readlineSync.question('Question: ' + answer);
-    switch (randomSymbols){
-        case symbol === '-':
-            answer = numbers1 - numbers2;
+    console.log(`Question: ${numbers1} ${symbol} ${numbers2}`);
+    const userAnswer = readlineSync.question('');
+    switch (symbol){
+        case '-':
+            correctAnswer = numbers1 - numbers2;
             i+=1;
         break;
-        case symbol === '+':
-            answer = numbers1 + numbers2;
+        case '+':
+            correctAnswer = numbers1 + numbers2;
             i+=1;
         break;
-        case symbol === '*':
-            answer = numbers1 * numbers2;
+        case '*':
+            correctAnswer = numbers1 * numbers2;
             i+=1;
         break;
         default:
             break;
     }
-    if (userAnswer === answer){
+    if (userAnswer === correctAnswer.toString()){
         console.log('Correct!');
-        i+=1;
     } else {
-        return console.log(userAnswer + ' is wrong answer ;(. Correct answer was ' + answer +'. Lets try again, ' + userName + '!')
+        return console.log(userAnswer + ' is wrong answer ;(. Correct answer was ' + correctAnswer +'. Lets try again, ' + userName + '!')
     }
 } while (i < 3);
 if (i = 3){
