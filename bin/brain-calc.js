a70dde4365e5144cd2a7c01a05be09b99dd3a3e2
delon <nistated@gmail.com>
2023-10-23 19:08:52 +0300

    add bin/brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,7 +7,7 @@ let pointsForCycle = 0;
 let correctAnswer = 0;
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+console.log(`Hello, ${userName}!`);
 console.log('What is the result of the expression?');
 const symbols = ['-', '+', '*'];
 do {
@@ -16,27 +16,27 @@ do {
   const symbol = randomSymbols(symbols);
   console.log(`Question: ${numbers1} ${symbol} ${numbers2}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  switch (symbol){
-      case '-':
-          correctAnswer = numbers1 - numbers2;
-      break;
-      case '+':
-          correctAnswer = numbers1 + numbers2;
-      break;
-      case '*':
-          correctAnswer = numbers1 * numbers2;
-      break;
-      default:
-          break;
+  switch (symbol) {
+  case '-':
+    correctAnswer = numbers1 - numbers2;
+    break;
+  case '+':
+    correctAnswer = numbers1 + numbers2;
+    break;
+  case '*':
+    correctAnswer = numbers1 * numbers2;
+    break;
+  default:
+    break;
   }
-  if (userAnswer === correctAnswer.toString()){
-      console.log('Correct!');
-      pointsForCycle+=1;
+  if (userAnswer === correctAnswer.toString()) {
+    console.log('Correct!');
+    pointsForCycle += 1;
   } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
-      break;
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
+    break;
   }
 } while (pointsForCycle < 3);
 if (pointsForCycle === 3){
-    console.log ('Congratulations, ' + userName + '!');
+  console.log ('Congratulations, ' + userName + '!');
 }
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,39 +1,37 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import {number} from './system_functions/functionofRandom.js'
+import { number } from './system_functions/functionofRandom.js';
+
 let pointsForCycle = 0;
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-console.log('What number is missing in the progression?');
-do  {
+console.log(`Hello, ${userName}!`);
+const firstNumber = number(1, 100);
+const step = number(1, 10);
+const result = [];console.log('What number is missing in the progression?');
+function array() { 
+  result[0] = firstNumber;
+  for (let i = 1; i < arrayLenght; i += 1){
+    result[i] = result[i - 1] + step;
+  }
+  return result;
+};
+do {
   const arrayLenght = number(5, 10);
-  const firstNumber = number(1, 100);
-  const step = number(1, 10);
-  let result = [];
-  function array(){ 
-    result[0] = firstNumber;
-    for (let i = 1; i < arrayLenght; i++){
-      result[i] = result[i - 1] + step;
-    }
-    return result;
-  };
   const arr = array();
   const index = Math.abs(arrayLenght - number (1, 9));
   const hiddenObj = arr[index];
   arr[index] = "..";
-  console.log('Question: ' + String(arr.join(' ')));
+  console.log(`Question: ${String(arr.join(' '))}`);
   const userAnswer = readlineSync.question('Your answer: ');
   if (userAnswer === hiddenObj.toString()){
     console.log('Correct!');
-    pointsForCycle+=1;
+    pointsForCycle += 1;
   } else {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenObj}'. \nLet's try again, ${userName}!`);
     break;
   }
-}while (pointsForCycle < 3);
+} while (pointsForCycle < 3);
 if (pointsForCycle === 3){
   console.log ('Congratulations, ' + userName + '!');
 }
-
-
