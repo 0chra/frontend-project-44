afb6c2a0d1e86308eb1df892504fbf4078b6532e
delon <nistated@gmail.com>
2023-10-23 12:27:14 +0300

    git add bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import {number} from './system_functions/functionofRandom.js';
+import { number } from './system_functions/functionofRandom.js';
 import { randomSymbols } from './system_functions/functionRandomSymblos.js';
+
 let pointsForCycle = 0;
 let correctAnswer = 0;
 console.log('Welcome to the Brain Games!');
@@ -10,31 +11,31 @@ console.log('Hello, ' + userName + '!');
 console.log('What is the result of the expression?');
 const symbols = ['-', '+', '*'];
 do {
-    const numbers1 = number(1, 100);
-    const numbers2 = number(1, 100);
-    const symbol = randomSymbols(symbols);
-    console.log(`Question: ${numbers1} ${symbol} ${numbers2}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    switch (symbol){
-        case '-':
-            correctAnswer = numbers1 - numbers2;
-        break;
-        case '+':
-            correctAnswer = numbers1 + numbers2;
-        break;
-        case '*':
-            correctAnswer = numbers1 * numbers2;
-        break;
-        default:
-            break;
-    }
-    if (userAnswer === correctAnswer.toString()){
-        console.log('Correct!');
-        pointsForCycle+=1;
-    } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
-        break;
-    }
+  const numbers1 = number(1, 100);
+  const numbers2 = number(1, 100);
+  const symbol = randomSymbols(symbols);
+  console.log(`Question: ${numbers1} ${symbol} ${numbers2}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  switch (symbol){
+      case '-':
+          correctAnswer = numbers1 - numbers2;
+      break;
+      case '+':
+          correctAnswer = numbers1 + numbers2;
+      break;
+      case '*':
+          correctAnswer = numbers1 * numbers2;
+      break;
+      default:
+          break;
+  }
+  if (userAnswer === correctAnswer.toString()){
+      console.log('Correct!');
+      pointsForCycle+=1;
+  } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
+      break;
+  }
 } while (pointsForCycle < 3);
 if (pointsForCycle === 3){
     console.log ('Congratulations, ' + userName + '!');
