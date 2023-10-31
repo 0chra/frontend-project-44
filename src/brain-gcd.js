effe871a8ccb40063decebf30eff71f8b2dd0d68
delon <nistated@gmail.com>
2023-10-31 19:16:49 +0300

    add bin/brain-gcd.js

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -1,25 +1,16 @@
 import engine from './index.js';
 import number from './system_functions/functionofRandom.js';
 
-export default function brainGcd() {
-  function nod(...theArgs) {
-    const arg = theArgs[0];
-    const argLenght = theArgs.length;
-    let pointNOD = arg;
-    for (let pointsFor = 1; pointsFor < argLenght; pointsFor += 1) {
-      const argPoints = theArgs[pointsFor];
-      let pointsComparison = argPoints;
-      while (pointNOD && pointsComparison) {
-        if (pointNOD > pointsComparison) {
-          pointNOD %= pointsComparison;
-        } else {
-          pointsComparison %= pointNOD;
-        }
-      }
-      pointNOD += pointsComparison;
-    }
-    return pointNOD;
+function nod(firstNumber, secondNumber) {
+  if (secondNumber > firstNumber) {
+    return nod(secondNumber, firstNumber);
   }
+  if (!secondNumber){
+    return firstNumber;
+  }
+  return nod(secondNumber, firstNumber % secondNumber);
+}
+export default function brainGcd() {
   const numbers1 = number(1, 100);
   const numbers2 = number(1, 100);
   const question = (`${numbers1} ${numbers2}`);
