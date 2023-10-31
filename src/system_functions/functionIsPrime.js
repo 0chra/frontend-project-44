76c88802fa1f7bc4620326667e57b096a796ebe4
delon <nistated@gmail.com>
2023-10-31 19:23:28 +0300

    add bin/brain-prime.js

--- src/system_functions/functionIsPrime.js
+++ src/system_functions/functionIsPrime.js
@@ -2,16 +2,18 @@ export default function isPrime(number) {
   if (number === 1) {
     const isTrue = false;
     return isTrue;
-  } else if (number === 2) {
+  } 
+  if (number === 2) {
     const isTrue = true;
     return isTrue;
-  } else {
+  } 
+  if (number !== 1 && number !== 2) {
     for (let x = 2; x < number; x += 1) {
       if (number % x === 0) {
         const isTrue = false;
         return isTrue;
       }
     }
-    return true;
   }
+  return true;
 }
