ee0c79272f2122d92062965cda63fe6e9d12ddcc
delon <nistated@gmail.com>
2023-11-07 17:23:50 +0300

    add games/brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,10 +1,13 @@
 import getRandomNumber from './getRandomNumber.js';
 import engine from './index.js';
 
+function isEven(number) {
+  return number % 2 === 0 ? 'yes' : 'no';
+}
 function getData() {
   const randomNumber = getRandomNumber(1, 100);
   const question = randomNumber;
-  const trueAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+  const trueAnswer = isEven(randomNumber);
   return [trueAnswer, question];
 }
 export default function brainEven() {
